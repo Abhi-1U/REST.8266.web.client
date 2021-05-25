d89aec5c554656603f2f4878e6d5c740a23275a7
Abhi-1U <PerricoQ@outlook.com>
2021-05-25 22:52:48 +0530

    GNU-Linux/Unix build script

---
+++ build.sh
@@ -0,0 +1,5 @@
+mkdocs build
+mkdir site/wr
+cp custom_theme/assets/js/FileSaver.js site/wr/FileSaver.js
+cp custom_theme/assets/js/term.js site/wr/term.js
+cp custom_theme/webrepl.html site/wr/webrepl.html
